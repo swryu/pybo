d319fbfeb7f6b6e9eebeab92b16c3328155f195f
Seungwoo Ryu <evilryu@naver.com>
2022-01-07 22:55:43 +0900

    URL 과 네임스페이스

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-        path('', views.index),
-        path('<int:question_id>/', views.detail),
+        path('', views.index, name='index'),
+        path('<int:question_id>/', views.detail, name='detail'),
 ]
